1085f05121157bf4eaffd1e48b49c65b794c2a4d
Rama Bondan Prakoso <ramanarubp@gmail.com>
2026-04-02 13:42:34 +0000

    ipv6: ip6_tunnel: fix rx stats update in vlan prepare error path

--- net/ipv6/ip6_tunnel.c
+++ net/ipv6/ip6_tunnel.c
@@ -880,8 +880,8 @@ static int __ip6_tnl_rcv(struct ip6_tnl *tunnel, struct sk_buff *skb,
 	skb_reset_network_header(skb);
 
 	if (!skb_vlan_inet_prepare(skb, true)) {
-		DEV_STATS_INC(tunnel->dev, rx_length_errors);
-		DEV_STATS_INC(tunnel->dev, rx_errors);
+		tunnel->dev->stats.rx_length_errors++;
+		tunnel->dev->stats.rx_errors++;
 		goto drop;
 	}
 
